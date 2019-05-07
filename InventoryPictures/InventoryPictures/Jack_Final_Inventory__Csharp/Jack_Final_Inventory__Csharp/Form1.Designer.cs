403fe344feb48e5a0cd08711d8c4edf81fc4b2a0
Jack Bobo <jackbobo50@gmail.com>
2019-05-07 06:21:36 -0400

    Inventory Pictures

---
+++ InventoryPictures/InventoryPictures/Jack_Final_Inventory__Csharp/Jack_Final_Inventory__Csharp/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Jack_Final_Inventory__Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.cmbSearch = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProdID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.computerInventoryDataSet = new Jack_Final_Inventory__Csharp.ComputerInventoryDataSet();
+            this.computersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.computersTableAdapter = new Jack_Final_Inventory__Csharp.ComputerInventoryDataSetTableAdapters.ComputersTableAdapter();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerInventoryDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(310, 525);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 40);
+            this.btnEdit.TabIndex = 33;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(436, 525);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 40);
+            this.btnCancel.TabIndex = 34;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(190, 525);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 40);
+            this.btnNew.TabIndex = 32;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // cmbSearch
+            // 
+            this.cmbSearch.FormattingEnabled = true;
+            this.cmbSearch.Location = new System.Drawing.Point(190, 4);
+            this.cmbSearch.Name = "cmbSearch";
+            this.cmbSearch.Size = new System.Drawing.Size(457, 28);
+            this.cmbSearch.TabIndex = 36;
+            this.cmbSearch.SelectedIndexChanged += new System.EventHandler(this.cmbSearch_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(77, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 20);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Search Item:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(572, 525);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 40);
+            this.btnSave.TabIndex = 35;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.label7);
+            this.panel.Controls.Add(this.pictureBox);
+            this.panel.Controls.Add(this.txtQuantity);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.label5);
+            this.panel.Controls.Add(this.txtProdID);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.txtPrice);
+            this.panel.Controls.Add(this.txtName);
+            this.panel.Controls.Add(this.label4);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.txtDescription);
+            this.panel.Enabled = false;
+            this.panel.Location = new System.Drawing.Point(31, 58);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(616, 434);
+            this.panel.TabIndex = 37;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(13, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Picture:";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.DataBindings.Add(new System.Windows.Forms.Binding("Image", this.computersBindingSource, "Picture", true));
+            this.pictureBox.Location = new System.Drawing.Point(119, 14);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(300, 198);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.computersBindingSource, "Quantity", true));
+            this.txtQuantity.Location = new System.Drawing.Point(119, 396);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(190, 26);
+            this.txtQuantity.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 402);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Quantity";
+            // 
+            // txtProdID
+            // 
+            this.txtProdID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.computersBindingSource, "Product ID", true));
+            this.txtProdID.Location = new System.Drawing.Point(119, 218);
+            this.txtProdID.Name = "txtProdID";
+            this.txtProdID.Size = new System.Drawing.Size(337, 26);
+            this.txtProdID.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.computersBindingSource, "Price", true));
+            this.txtPrice.Location = new System.Drawing.Point(119, 364);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(190, 26);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.computersBindingSource, "Name", true));
+            this.txtName.Location = new System.Drawing.Point(119, 250);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(337, 26);
+            this.txtName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Price";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.computersBindingSource, "Description", true));
+            this.txtDescription.Location = new System.Drawing.Point(119, 282);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(476, 75);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // computerInventoryDataSet
+            // 
+            this.computerInventoryDataSet.DataSetName = "ComputerInventoryDataSet";
+            this.computerInventoryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // computersBindingSource
+            // 
+            this.computersBindingSource.DataMember = "Computers";
+            this.computersBindingSource.DataSource = this.computerInventoryDataSet;
+            // 
+            // computersTableAdapter
+            // 
+            this.computersTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 569);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.cmbSearch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.panel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(709, 625);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(709, 625);
+            this.Name = "Form1";
+            this.Text = "Inventory Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerInventoryDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.ComboBox cmbSearch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProdID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private ComputerInventoryDataSet computerInventoryDataSet;
+        private System.Windows.Forms.BindingSource computersBindingSource;
+        private ComputerInventoryDataSetTableAdapters.ComputersTableAdapter computersTableAdapter;
+    }
+}
+
---
+++ InventoryPictures/InventoryPictures/Jack_Final_Inventory__Csharp/Jack_Final_Inventory__Csharp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jack_Final_Inventory__Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            // TODO: This line of code loads data into the 'computerInventoryDataSet.Computers' table. You can move, or remove it, as needed.
+            this.computersTableAdapter.Fill(this.computerInventoryDataSet.Computers);
+            computersBindingSource.DataSource = this.computerInventoryDataSet.Computers;
+
+
+
+
+            var query = from o in this.computerInventoryDataSet.Computers
+                        where o.Product_ID.Contains(cmbSearch.Text) || o.Name == cmbSearch.Text || o.Description == cmbSearch.Text || o.Price == cmbSearch.Text || o.Quantity.Contains(cmbSearch.Text)
+                        select o;
+            //computersBindingSource1.DataSource = query.ToList();
+            cmbSearch.DataSource = query.ToList();
+            cmbSearch.DisplayMember = "Name";
+            cmbSearch.ValueMember = "Product_ID";
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                txtProdID.Focus();
+                this.computerInventoryDataSet.Computers.AddComputersRow(this.computerInventoryDataSet.Computers.NewComputersRow());
+                computersBindingSource.ResetBindings(false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                computersBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            txtProdID.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+            panel.Enabled = false;
+            computersBindingSource.ResetBindings(false);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                computersBindingSource.EndEdit();
+                computersTableAdapter.Update(this.computerInventoryDataSet.Computers);
+                panel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                computersBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void cmbSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            computersBindingSource.Position = cmbSearch.SelectedIndex;
+
+
+            if (cmbSearch.SelectedIndex == 0)
+            {
+                pictureBox.Image = Properties.Resources.Flex5_14;
+            }
+            if (cmbSearch.SelectedIndex == 1)
+            {
+                pictureBox.Image = Properties.Resources.Lenovoideapad_320_156;
+            }
+            if (cmbSearch.SelectedIndex == 2)
+            {
+                pictureBox.Image = Properties.Resources.LenovoIdeaPad_173;
+            }
+            if (cmbSearch.SelectedIndex == 3)
+            {
+                pictureBox.Image = Properties.Resources.Yoga_920_14;
+            }
+            if (cmbSearch.SelectedIndex == 4)
+            {
+                pictureBox.Image = Properties.Resources.Miix320;
+            }
+            if (cmbSearch.SelectedIndex == 5)
+            {
+                pictureBox.Image = Properties.Resources.Flex5_14;
+            }
+            if (cmbSearch.SelectedIndex == 6)
+            {
+                pictureBox.Image = Properties.Resources.Lenovoideapad_320_156;
+            }
+            if (cmbSearch.SelectedIndex == 7)
+            {
+                pictureBox.Image = Properties.Resources.Yoga910_14;
+            }
+            if (cmbSearch.SelectedIndex == 8)
+            {
+                pictureBox.Image = Properties.Resources.Flex5_14;
+            }
+            if (cmbSearch.SelectedIndex == 9)
+            {
+                pictureBox.Image = Properties.Resources.lenovohero;
+            }
+        }
+    }
+    }
+
+
